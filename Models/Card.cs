e1520e3472be6c9f92bfebf75546cc3da8a674ef
AndrewMc8 <78245384+AndrewMc8@users.noreply.github.com>
2022-02-11 08:46:40 -0700

    Card model

---
+++ Models/Card.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Digital_Pocket_Monster.Models
+{
+    public class Card
+    {
+        public string name { get; set; }
+        public string cardType { get; set; }
+        public string color { get; set; }
+        public int level { get; set; }
+        public string cardNumber { get; set; }
+
+        public Card()
+        {
+
+        }
+
+        public Card(string name, string cardType, string color, int level, string cardNumber)
+        {
+            this.name = name;
+            this.cardType = cardType;
+            this.color = color;
+            this.level = level;
+            this.cardNumber = cardNumber;
+        }
+        public override string ToString()
+        {
+            return $"{name} - {cardType} - {color} - {level} - {cardNumber}";
+        }
+    }
+}
