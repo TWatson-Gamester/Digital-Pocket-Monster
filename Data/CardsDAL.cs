1e7f7801492d8272c7b90216f3b96528ed0dff83
Ghostninja2451 <80051664+Ghostninja2451@users.noreply.github.com>
2022-02-11 09:51:29 -0700

    Added the database work

---
+++ Data/CardsDAL.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Digital_Pocket_Monster.Interface;
+using Digital_Pocket_Monster.Models;
+
+namespace Digital_Pocket_Monster.Data 
+{
+    public class CardsDAL : IDataAccessLayer
+    {
+        public void addCard(Card card)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Card> filterCards(string color, string cardType, int level)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Card getCard(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void removeCard(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Card> searchCards(string searchCard)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Card> showCards()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
