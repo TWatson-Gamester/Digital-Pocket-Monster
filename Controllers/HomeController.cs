0099c267b34d956fd463d0f2b66441a95140299e
AlaynaGarry <Alaynagarry@gmail.com>
2022-02-22 08:26:40 -0700

    Header Update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -35,6 +35,10 @@ namespace Digital_Pocket_Monster.Controllers
         {
             return View();
         }
+        public IActionResult Deck()
+        {
+            return View();
+        }
         public IActionResult Collection()
         {
             return View(dal.showCards());
