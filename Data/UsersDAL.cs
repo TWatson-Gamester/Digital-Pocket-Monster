9eec016eb36eb31baef5c8fc42215349a9bd8c90
AndrewMc8 <78245384+AndrewMc8@users.noreply.github.com>
2022-02-23 09:39:54 -0700

    Made UsersDAL

---
+++ Data/UsersDAL.cs
@@ -0,0 +1,40 @@
+﻿using Digital_Pocket_Monster.Interface;
+using Digital_Pocket_Monster.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Digital_Pocket_Monster.Data
+{
+    public class UsersDAL : IDataAccessLayerUsers
+    {
+        private DigimonContext db;
+        public UsersDAL(DigimonContext indb)
+        {
+            this.db = indb;
+        }
+
+        public Deck getDeck(int? id)
+        {
+            Deck foundDeck = null;
+
+            if (id != null)
+            {
+                db.Decks.ToList().ForEach(d =>
+                {
+                    if (d.ID == id)
+                    {
+                        foundDeck = d;
+                    }
+                });
+            }
+            return foundDeck;
+        }
+
+        public IEnumerable<Deck> showDecks(string userId)
+        {
+            return db.Decks.Where(m => m.UserID == userId).ToList();
+        }
+    }
+}
