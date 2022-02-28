7015b83f35b738d1c8ed7d5a807652eb233018ad
TWatson-Gamester <twatson@student.neumont.edu>
2022-02-28 09:37:54 -0700

    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH Deck Builder Controller now in testing phase with View

--- Controllers/DeckBuilderController.cs
+++ Controllers/DeckBuilderController.cs
@@ -1,11 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Digital_Pocket_Monster.Models;
 using Digital_Pocket_Monster.Data;
 using Digital_Pocket_Monster.Interface;
-using Digital_Pocket_Monster.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 namespace Digital_Pocket_Monster.Controllers
 {
@@ -13,11 +17,17 @@ namespace Digital_Pocket_Monster.Controllers
     {
         private int digiEggs = 0;
         private int cardsInDeck = 0;
-        private DecksDAL decksDAL;
+        IDataAccessLayerDecks decksDAL;
+
+        public DeckBuilderController(IDataAccessLayerDecks inDB)
+        {
+            decksDAL = inDB;
+        }
+
 
         public IActionResult DeckBuilding()
         {
-            return View();
+            return View("DeckBuilding", decksDAL.getDeck(1));
         }
 
         [HttpPost]
@@ -25,12 +35,13 @@ namespace Digital_Pocket_Monster.Controllers
         {
             bool canAdd = true;
             Card cardBeingAdded = decksDAL.getCard(cardNumber);
-/*
-            if(decksDAL.getCard(cardNumber) >= 4){
-                canAdd = false;
-             }*/
 
-            if(cardBeingAdded.CardType == "Digitama" && digiEggs < 5 && canAdd)
+/*            if (decksDAL.getCard(cardNumber) >= 4)
+            {
+                canAdd = false;
+            }   Make something that searches and looks for how many of a card there is*/   
+
+            if (cardBeingAdded.CardType == "Digitama" && digiEggs < 5 && canAdd)
             {
                 digiEggs++;
             }
@@ -45,7 +56,7 @@ namespace Digital_Pocket_Monster.Controllers
                 cardsInDeck++;
             }
 
-            return View("DeckBuilder");
+            return View("DeckBuilding", decksDAL.getDeck(1));
         }
 
         [HttpPost]
@@ -60,7 +71,7 @@ namespace Digital_Pocket_Monster.Controllers
             //decksDAL.removeCard(1, cardBeingRemoved);
             cardsInDeck--;
 
-            return View("DeckBuilder");
+            return View("DeckBuilding", decksDAL.getDeck(1));
         }
 
     }
